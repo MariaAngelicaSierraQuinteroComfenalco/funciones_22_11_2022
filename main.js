a87135a444466964d31acfba0d075a20f17fc0e6
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-22 21:33:26 -0500

    funcion anonima

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 // https://lenguajejs.com/javascript/fundamentos/funciones/
 // http://www.etnassoft.com/2016/06/22/las-funciones-flecha-en-javascript-parte-1/
 // https://www.youtube.com/watch?v=MiH3pbP4EFc&ab_channel=FernandoHerrera
-addEventListener("DOMContentLoaded", (e)=>{
+addEventListener("DOMContentLoaded", (e) => {
 
-    //La funcion por expresion es una de las habituales, en esta se guarda
-    // una funcion dentro de una varible con esta podemos lograr menos lineas de codigo
-        const saludo = function saludar() {
-        return "Hola";
-         };
-    //Para poder ejecutar la funcion debemos utlizar el nombre de la varaible ya que el
-    //nombre de la funcion no existira
-        saludo(); 
-        console.log(saludo());
-    //a esto tambien se le conoce como funciones anonimas 
-    
-    })
+    //La funcion anonima son las declaraciones de funciones sin nombre pero estando dentro de una variable
+    const saludo = function () {
+      return "Hola";
+    };
+    //Para poder ejecutar la funcion debemos utlizar el nombre de la variable ya que el
+    //nombre de la funcion no lo tenfremos
+    saludo; // al ponerlas sin parentesis nos devulve la funcion completa ƒ () { return 'Hola'; }
+    saludo(); //pero si esta con parentesis nos muestra el retorno de la funcion 
+  
+    console.log(saludo);//poder visualizar en consola
+    console.log(saludo());
+  
+  })
