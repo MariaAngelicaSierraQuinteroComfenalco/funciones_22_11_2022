74d2c789ed033e4b8a2a43581737170cb3dbf0a6
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-22 20:30:04 -0500

    La funcion por expresion

--- main.js
+++ main.js
@@ -2,12 +2,16 @@
 // http://www.etnassoft.com/2016/06/22/las-funciones-flecha-en-javascript-parte-1/
 // https://www.youtube.com/watch?v=MiH3pbP4EFc&ab_channel=FernandoHerrera
 addEventListener("DOMContentLoaded", (e)=>{
- 
-    //La funcion como objeto no estan utlizada pero es interesante
-    //Esta utilizamos la creacion de un objeto y la funcion juntas 
-    const saludar = new Function("return 'Hola';");
-    //el nombre a utlizar seria el de la variable 
-    saludar(); // 'Hola'
-    console.log(saludar());
 
-})
+    //La funcion por expresion es una de las habituales, en esta se guarda
+    // una funcion dentro de una varible con esta podemos lograr menos lineas de codigo
+        const saludo = function saludar() {
+        return "Hola";
+         };
+    //Para poder ejecutar la funcion debemos utlizar el nombre de la varaible ya que el
+    //nombre de la funcion no existira
+        saludo(); 
+        console.log(saludo());
+    //a esto tambien se le conoce como funciones anonimas 
+    
+    })
