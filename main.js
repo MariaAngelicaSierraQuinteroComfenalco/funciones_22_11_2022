57f0e7d84b0f99d2fe15f98f1edb06d33e68db64
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-22 19:59:23 -0500

    funciones_22_11_2022

---
+++ main.js
@@ -0,0 +1,17 @@
+// https://lenguajejs.com/javascript/fundamentos/funciones/
+// http://www.etnassoft.com/2016/06/22/las-funciones-flecha-en-javascript-parte-1/
+// https://www.youtube.com/watch?v=MiH3pbP4EFc&ab_channel=FernandoHerrera
+addEventListener("DOMContentLoaded", (e)=>{
+ 
+    
+    function saludar() {
+        return "Hola";
+      }
+      
+      saludar(); // 'Hola'
+      typeof saludar; // 'function'
+
+    console.log(saludar());
+    
+
+})
