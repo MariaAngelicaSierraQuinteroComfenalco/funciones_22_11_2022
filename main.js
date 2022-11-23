f29c0b0014d82a68c0528086b0e1326d7652fc55
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-22 20:06:51 -0500

    Funciones por declaración

--- main.js
+++ main.js
@@ -3,12 +3,16 @@
 // https://www.youtube.com/watch?v=MiH3pbP4EFc&ab_channel=FernandoHerrera
 addEventListener("DOMContentLoaded", (e)=>{
  
-    
+    //La funcion por declaracion es de las mas utlizadas ya que esta 
+    //estara declarara para poder utlizarla a lo largo del codigo
     function saludar() {
         return "Hola";
       }
       
-      saludar(); // 'Hola'
+      saludar(); //al poner en nombre de la funcion le decimos que ejecute 
+       // podriamos ponerlo antes de haber creado la funcion porque 
+      //sin embargo javascript leera primero las funciones sin importar el orden 
+     
       typeof saludar; // 'function'
 
     console.log(saludar());
