2c3760d58dfe77f9e326cf30a00ef944a600f88b
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-24 20:19:18 -0500

    callbacks

--- main.js
+++ main.js
@@ -3,20 +3,21 @@
 // https://www.youtube.com/watch?v=MiH3pbP4EFc&ab_channel=FernandoHerrera
 addEventListener("DOMContentLoaded", (e) => {
 
-//Las Arrow functions son funciones flechas es decir no se utliza la palabra function
-// sino de utiliza => antes de las llaves 
-  const func = function () {
-    return "Función tradicional.";
-  };
-  // funcion flechas 
-  const fun= () => {
-    return "Función flecha.";
-  };
-// estas tiene una ventaja ya que se pueden simplificar varias cosas como
-//si el cuerpo es de una sola linea no llevara llaves y si sucede no es necesario el return
-// en caso que la funcion no tenga parametros seria asi  () =>
-//si solo tiene uno se puede indicar con el nombre del mismo e =>
-// en casi de 2 o mas parametros seria asi (a, b) =>
+//un callback  es pasar una función B por parámetro a una función A
+// es decir al ejecutar la funcion A se pueda tambien ejecutar la funcion B
+// ademas se puede  definen en variables 
+
+// fB = Función B
+const fB = function () {
+  console.log("Función B ejecutada.");
+};
+
+// fA = Función A
+const fA = function (callback) {
+  callback();
+};
+
+fA(fB);
 
 })
 
