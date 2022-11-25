ae768c998f7bd353ca41fd5a9240dae2fef675d9
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-24 19:47:59 -0500

    Clausuras

--- main.js
+++ main.js
@@ -3,29 +3,26 @@
 // https://www.youtube.com/watch?v=MiH3pbP4EFc&ab_channel=FernandoHerrera
 addEventListener("DOMContentLoaded", (e) => {
 
-    //La funcion autoejecutables son funciones anonimas que se ejecutan mediante la marcha del codigo
-//lo unico que necesitamos es envolver la funcion anonima entre parentesis 
-
-// Función autoejecutable
-(function () {
-  console.log("Hola!!");
+// una clausura se podria definir como una funcion que encierra las variables 
+//la variable es incr esta guardando lo que devuelve la funcion es uan funcion anonima y autoejecutable que tiene clausura 
+//estamos creando dentro de la funcion una varible que va aumentando el valor y devolverlo 
+// Clausura: Función incr()
+const incr = (function () {
+  let num = 0; // la declaramos en cero
+  return function () {
+  num++;// hace que vaya aumentando 
+  return num;//retorna el valor
+  };
 })();
 
+typeof incr; // 'function'
+incr(); // 1
+incr(); // 2
+incr(); // 3
 
-// Función autoejecutable con parámetros
-(function (name) {
-  console.log(`¡Hola, ${name}!`);
-})("Manz");
-//no es necesario poner nombre a la funcion ya que es anonima 
-
-//al hacer un return lo que almacena la variable es el valor de la funcion 
-const f = (function (name) {
-  return `¡Hola, ${name}!`;
-})("Manz");
-
-f; // '¡Hola, Manz!`
-typeof f; // 'string'
-
-
+console.log(incr());
+console.log(incr());
+console.log(incr());
+console.log(incr());
 })
 
